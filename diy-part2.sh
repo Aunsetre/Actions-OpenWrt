bd5c122e212933b6a08f63c5b12f68fd0af1f908
Aunsetre <aunsetre@outlook.com>
2020-05-03 17:48:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,52 @@
 #============================================================
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.31.2/192.168.50.5/g' package/base-files/files/bin/config_generatei
+
+cd package/lean/  
+
+rm -rf luci-app-vssr  luci-lib-docker luci-app-dockerman luci-theme-argon luci-app-smartdns luci-app-adguardhome openwrt-packages
+rm -rf openwrt-kcptun luci-app-kcptun lua-maxminddb small
+
+# adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+
+# luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+
+#docker
+git clone https://github.com/lisaac/luci-app-dockerman.git
+git clone https://github.com/lisaac/luci-lib-docker.git
+
+#kcptun
+https://github.com/kuoruan/openwrt-kcptun.git
+git clone https://github.com/kuoruan/luci-app-kcptun.git 
+
+# helloWorld
+git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
+git clone https://github.com/jerrykuku/luci-app-vssr.git 
+
+# passwall 依赖
+ git clone https://github.com/kenzok8/small
+
+#luci-app-openclash ------------------openclash图形
+# luci-app-advancedsetting ------------------系统高级设置
+# luci-theme-atmaterial ------------------atmaterial 三合一主题（适配18.06）
+# luci-app-aliddns ------------------阿里云ddns
+# luci-theme-argon-dark-new------------------适配19.07与18.06的主题
+# luci-app-eqos ------------------依IP地址限速
+# luci-app-gost ------------------隐蔽的https代理
+# luci-app-koolproxyR ------------------去广告
+# luci-app-smartdns ------------------smartdns防污染
+# luci-app-passwall ------------------Lienol大神
+# luci-theme-argon_new ------------------适配19.07与18.06的主题
+# luci-app-ssr-plus ------------------Lean大神
+# luci-theme-opentomcat ------------------修复主机名错误（适配18.06）
+# luci-theme-opentomato ------------------修复主机名错误（适配18.06）
+# 注意
+ git clone https://github.com/kenzok8/openwrt-packages.git
+ cd openwrt-packages/
+ rm -rf luci-app-smartdns/
+ cd ..  
+ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
+
