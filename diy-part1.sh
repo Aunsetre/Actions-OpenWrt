267e9d20d2962f57b13b31ccc34ecd29cb416443
Aunsetre <aunsetre@outlook.com>
2020-05-03 21:19:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,47 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+ 
+ cd package/lean/  
+
+# adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+
+# luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+
+#docker
+git clone https://github.com/lisaac/luci-app-dockerman.git
+git clone https://github.com/lisaac/luci-lib-docker.git
+
+#kcptun
+https://github.com/kuoruan/openwrt-kcptun.git
+git clone https://github.com/kuoruan/luci-app-kcptun.git 
+
+# helloWorld
+git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
+git clone https://github.com/jerrykuku/luci-app-vssr.git 
+
+# passwall 依赖
+ git clone https://github.com/kenzok8/small
+
+#luci-app-openclash ------------------openclash图形
+# luci-app-advancedsetting ------------------系统高级设置
+# luci-theme-atmaterial ------------------atmaterial 三合一主题（适配18.06）
+# luci-app-aliddns ------------------阿里云ddns
+# luci-theme-argon-dark-new------------------适配19.07与18.06的主题
+# luci-app-eqos ------------------依IP地址限速
+# luci-app-gost ------------------隐蔽的https代理
+# luci-app-koolproxyR ------------------去广告
+# luci-app-smartdns ------------------smartdns防污染
+# luci-app-passwall ------------------Lienol大神
+# luci-theme-argon_new ------------------适配19.07与18.06的主题
+# luci-app-ssr-plus ------------------Lean大神
+# luci-theme-opentomcat ------------------修复主机名错误（适配18.06）
+# luci-theme-opentomato ------------------修复主机名错误（适配18.06）
+# 注意
+ git clone https://github.com/kenzok8/openwrt-packages.git
+ cd openwrt-packages/
+ rm -rf luci-app-smartdns/
+ cd ..  
+ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
