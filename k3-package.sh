1f09efff01212fb43d03d73fa72b9aa45f0bc4ed
Aunsetre <aunsetre@outlook.com>
2020-05-17 22:27:17 +0800

    Create k3-package.sh

---
+++ k3-package.sh
@@ -0,0 +1,13 @@
+# adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
+
+# luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+
+#docker
+git clone https://github.com/lisaac/luci-app-dockerman.git
+git clone https://github.com/lisaac/luci-lib-docker.git
+
+git clone https://github.com/lwz322/luci-app-k3screenctrl.git
+git clone https://github.com/lwz322/k3screenctrl.git
+git clone https://github.com/lwz322/k3screenctrl_build.git
