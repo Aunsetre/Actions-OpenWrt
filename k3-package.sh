5ca6ac6295b05916f634b4563500661199c1c71e
Aunsetre <aunsetre@outlook.com>
2020-05-24 22:42:47 +0800

    Update k3-package.sh

--- k3-package.sh
+++ k3-package.sh
@@ -1,11 +1,11 @@
 cd package/lean/
 # adguardhome
-rm -rf k3screenctrl  luci-app-dockerman luci-lib-docker
+rm -rf k3screenctrl  luci-app-dockerman luci-lib-docker luci-theme-argon
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 
 # luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
-sed -i 's/Microsoft Yahei/Sarasa UI SC/ ' luci-theme-argon/htdocs/luci-static/argon/css/style.css
+sed -i 's/Microsoft Yahei/Sarasa UI SC/' luci-theme-argon/htdocs/luci-static/argon/css/style.css
 
 #docker
 #git clone https://github.com/lisaac/luci-app-dockerman.git
