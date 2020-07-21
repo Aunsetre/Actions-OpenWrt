1e2af25bac7442f1f2f8057c8ee4f11463c63687
Aunsetre <aunsetre@outlook.com>
2020-07-21 13:19:13 +0800

    Update k3-package.sh

--- k3-package.sh
+++ k3-package.sh
@@ -1,6 +1,6 @@
 cd package/lean/
 # adguardhome
-rm -rf k3screenctrl  luci-app-dockerman luci-lib-docker luci-theme-argon
+#rm -rf k3screenctrl  luci-app-dockerman luci-lib-docker luci-theme-argon
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 
 # luci-theme-argon
@@ -11,6 +11,6 @@ sed -i 's/Microsoft Yahei/Sarasa UI SC/' luci-theme-argon/htdocs/luci-static/arg
 #git clone https://github.com/lisaac/luci-app-dockerman.git
 #git clone https://github.com/lisaac/luci-lib-docker.git
 
-git clone https://github.com/lwz322/luci-app-k3screenctrl.git
-git clone https://github.com/lwz322/k3screenctrl.git
-git clone https://github.com/lwz322/k3screenctrl_build.git
+#git clone https://github.com/lwz322/luci-app-k3screenctrl.git
+#git clone https://github.com/lwz322/k3screenctrl.git
+#git clone https://github.com/lwz322/k3screenctrl_build.git
