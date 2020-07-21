0c2293fbcd18b79e34018e27b4a2a6567257ad2a
Aunsetre <aunsetre@outlook.com>
2020-07-21 15:23:42 +0800

    Update k3-package.sh

--- k3-package.sh
+++ k3-package.sh
@@ -1,6 +1,6 @@
 cd package/lean/
 # adguardhome
-#rm -rf k3screenctrl  luci-app-dockerman luci-lib-docker luci-theme-argon
+rm -rf  luci-app-dockerman luci-lib-docker luci-theme-argon
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
 
 # luci-theme-argon
