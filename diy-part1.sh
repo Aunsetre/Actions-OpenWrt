1871d98271c68fde2ba4a9116fbf434292a8c03d
Aunsetre <aunsetre@outlook.com>
2021-04-06 11:13:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 #=============================================================
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 sed -i '$a src-git diy1 https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
@@ -34,9 +34,9 @@ git clone https://github.com/lisaac/luci-app-dockerman.git
 git clone https://github.com/lisaac/luci-lib-docker.git
 
 # helloWorld
-#git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
-#git clone https://github.com/garypang13/luci-app-bypass.git
-#git clone https://github.com/jerrykuku/luci-app-vssr.git 
+git clone https://github.com/jerrykuku/lua-maxminddb.git  #git lua-maxminddb 依赖
+git clone https://github.com/garypang13/luci-app-bypass.git
+git clone https://github.com/jerrykuku/luci-app-vssr.git 
 
 # passwall 依赖
 #brook  chinadns-ng   dns2socks  ipt2socks  kcptun  pdnsd-alt  simple-obfs  tcping  v2ray-plugin
